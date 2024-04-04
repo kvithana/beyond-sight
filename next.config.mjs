2ed710124c518cb4106bf8b56c54fa9504ee5fbf
Kalana Vithana <kalana@propps.com>
2024-04-04 17:49:02 +1100

    feat: yolo detection

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,49 @@
 /** @type {import('next').NextConfig} */
+import CopyPlugin from "copy-webpack-plugin";
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, {}) => {
+    config.resolve.extensions.push(".ts", ".tsx");
+    config.resolve.fallback = { fs: false };
+
+    config.plugins.push(
+      new NodePolyfillPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "./node_modules/onnxruntime-web/dist/ort-wasm.wasm",
+            to: "static/chunks/pages",
+          },
+          {
+            from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm",
+            to: "static/chunks/pages",
+          },
+          {
+            from: "./models",
+            to: "static/chunks/pages",
+          },
+        ],
+      })
+    );
+
+    return config;
+  },
 };
 
-export default nextConfig;
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
+
+export default nextConfigFunction;
