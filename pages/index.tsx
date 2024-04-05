bd41b2899044fc41ff7847a54c66e3a89872e3d2
Kalana Vithana <kalana@propps.com>
2024-04-05 21:49:15 +1100

    feat: try add to homescreen package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import Yolo from "@/components/models/yolo";
 import "@/controllers/init";
+import dynamic from "next/dynamic";
 import { Inter } from "next/font/google";
+import { Fragment } from "react";
+
+const AddToHomeScreen = dynamic(
+  () => import("@ideasio/add-to-homescreen-react"),
+  {
+    ssr: false,
+  }
+);
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Yolo />;
+  return (
+    <Fragment>
+      <AddToHomeScreen />
+      <Yolo />;
+    </Fragment>
+  );
 }
