6495c1371b973f4b19e7270c118691e6a8bc8517
Kalana Vithana <kalana@propps.com>
2024-04-05 16:34:51 +1100

    feat: add manifest
    Co-authored-by: Joyce-enactus <Joyce-enactus@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 import CopyPlugin from "copy-webpack-plugin";
-import {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} from "next/constants.js";
 import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
 const nextConfig = {
@@ -36,14 +32,4 @@ const nextConfig = {
   },
 };
 
-const nextConfigFunction = async (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const withPWA = (await import("@ducanh2912/next-pwa")).default({
-      dest: "public",
-    });
-    return withPWA(nextConfig);
-  }
-  return nextConfig;
-};
-
-export default nextConfigFunction;
+export default nextConfig;
