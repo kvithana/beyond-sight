a4f92bfd488e702ede1035a19a6583c53b4493ac
Kalana Vithana <kalana@propps.com>
2024-04-05 21:52:03 +1100

    Revert "feat: try add to homescreen package"
    This reverts commit bd41b2899044fc41ff7847a54c66e3a89872e3d2.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,9 @@
 import Yolo from "@/components/models/yolo";
 import "@/controllers/init";
-import dynamic from "next/dynamic";
 import { Inter } from "next/font/google";
-import { Fragment } from "react";
-
-const AddToHomeScreen = dynamic(
-  () => import("@ideasio/add-to-homescreen-react"),
-  {
-    ssr: false,
-  }
-);
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <Fragment>
-      <AddToHomeScreen />
-      <Yolo />;
-    </Fragment>
-  );
+  return <Yolo />;
 }
