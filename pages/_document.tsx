5c9693bc2ea60eb0c77f3b6a980d870214040832
Kalana Vithana <kalana@propps.com>
2024-04-05 16:39:15 +1100

    chore: add manifest tags for ios

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <link rel="manifest" href="manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-title" content="Beyond Sight" />
+      </Head>
       <body>
         <Main />
         <NextScript />
