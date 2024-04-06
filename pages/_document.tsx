ceedae602d3375591c637c4f61f52b3438093ae5
Kalana Vithana <kalana@propps.com>
2024-04-06 13:22:43 +1100

    feat: add title and description to header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,13 @@ export default function Document() {
         <link rel="manifest" href="manifest.json" />
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-title" content="Beyond Sight" />
+        <meta name="application-name" content="Beyond Sight" />
+        <meta name="title" content="Beyond Sight" />
+        <title key="title">Beyond Sight</title>
+        <meta
+          name="description"
+          content="An AI-powered lens into the world for the visually impaired."
+        />
       </Head>
       <body className="overscroll-none w-screen h-screen overflow-hidden margin-0">
         <Main />
