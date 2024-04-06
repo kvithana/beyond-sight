4442b51acc1b3badef0d38f1da95c61326f764b6
Kalana Vithana <kalana@propps.com>
2024-04-06 11:20:20 +1100

    feat: fix buttons

--- components/action-button.tsx
+++ components/action-button.tsx
@@ -1,9 +1,36 @@
-import { differenceInMilliseconds } from "date-fns";
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { useSwipeable } from "react-swipeable";
 
 export function ActionButton() {
   const [draggedAt, setDraggedAt] = useState<Date | null>(null);
+
+  const bind = useSwipeable({
+    onSwipedUp: () => {
+      console.log("swiped up");
+      new Howl({
+        src: ["/audio/query.mp3"],
+      }).play();
+    },
+    onSwipedDown: () => {
+      console.log("swiped down");
+    },
+    onSwipedLeft: () => {
+      console.log("swiped left");
+      new Howl({
+        src: ["/audio/success.mp3"],
+      }).play();
+    },
+    onSwipedRight: () => {
+      console.log("swiped right");
+      new Howl({
+        src: ["/audio/success.mp3"],
+      }).play();
+    },
+    onTap: () => {
+      console.log("tapped");
+    },
+  });
 
   return (
     <div className="fixed bottom-32 left-0 right-0 mb-4 w-screen flex items-center justify-center h-64 z-10">
@@ -13,26 +40,9 @@ export function ActionButton() {
         transition={{ duration: 1 }}
         className="h-72 w-72 rounded-full border-2 border-opacity-20 border-white flex items-center justify-center"
       >
-        <motion.button
-          drag
-          dragDirectionLock
-          onDirectionLock={(direction) => {
-            setDraggedAt(new Date());
-            console.log("direction", direction);
-          }}
-          className="h-64 w-64 rounded-full border-2 border-white"
-          dragSnapToOrigin
-          dragTransition={{ bounceStiffness: 700, bounceDamping: 30 }}
-          dragConstraints={{ bottom: 2, top: 2, left: 2, right: 2 }}
-          whileTap={{ scale: 0.9 }}
-          onTap={() => {
-            if (
-              draggedAt &&
-              differenceInMilliseconds(new Date(), draggedAt) < 500
-            )
-              return;
-            console.log("tapped");
-          }}
+        <button
+          {...bind}
+          className="h-64 w-64 rounded-full border-2 border-white touch-none"
         />
       </motion.div>
     </div>
