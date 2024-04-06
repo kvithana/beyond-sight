9fcdf0d6b65a51c7dcf60f1eb257dc589203b50f
Kalana Vithana <kalana@propps.com>
2024-04-06 14:17:11 +1100

    chore: wait 1 second before showing start screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 
 export default function Save() {
@@ -14,7 +15,13 @@ export default function Save() {
   }, []);
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center flex-col">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1, delay: 1 }}
+      className="h-screen w-screen flex items-center justify-center flex-col"
+    >
       <img
         src="/icon512_maskable.png"
         className="h-32 w-32 rounded-lg border-white border-4"
@@ -38,6 +45,6 @@ export default function Save() {
           Start seeing
         </a>
       </div>
-    </div>
+    </motion.div>
   );
 }
