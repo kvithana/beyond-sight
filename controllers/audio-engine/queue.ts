51851aabca45f326ddc92873b7bf0f9ffbdae91f
Kalana Vithana <kalana@propps.com>
2024-04-08 12:52:29 +1000

    fix: audio queue sould prioritise lowest first

--- controllers/audio-engine/queue.ts
+++ controllers/audio-engine/queue.ts
@@ -19,10 +19,10 @@ export interface QueuedTrack {
 
 const compare: ICompare<QueuedTrack> = (a: QueuedTrack, b: QueuedTrack) => {
   if (a.priority > b.priority) {
-    return -1;
+    return 1;
   }
   if (a.priority < b.priority) {
-    return 1;
+    return -1;
   }
   return 0;
 };
